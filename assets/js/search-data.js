1d508851a0f53ef91c4eeddce70fd62acae4b6f6
maxischaller <66011587+maxischaller@users.noreply.github.com>
2025-03-18 18:48:50 +0000

    Deploying to gh-pages from @ maxischaller/maxischaller.github.io@f9f96052d08ece4dc63cb03434d7aac679e84c63 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,13 +25,6 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/cv/";
-          },
-        },{id: "nav-cv",
-          title: "cv",
           description: "",
           section: "Navigation",
           handler: () => {
